5f0ba91e75d55b8cacc3169060730cd8b3a779b7
EC2 Default User <ec2-user@ip-172-31-34-26.ec2.internal>
2019-07-22 01:02:10 +0000

    Updates

---
+++ precache-manifest.1e4de3c9cd1f7ca091659fc5ba00be69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2491e33a1ba31dd939a82c78192826",
+    "url": "/country_guessing_reactapp/index.html"
+  },
+  {
+    "revision": "e82b4928ff32a0718695",
+    "url": "/country_guessing_reactapp/static/css/main.db5b3269.chunk.css"
+  },
+  {
+    "revision": "25703958c9ac55ca0af9",
+    "url": "/country_guessing_reactapp/static/js/2.1c1fd390.chunk.js"
+  },
+  {
+    "revision": "e82b4928ff32a0718695",
+    "url": "/country_guessing_reactapp/static/js/main.95bfb77c.chunk.js"
+  },
+  {
+    "revision": "faa0b39d1b4cefa19000",
+    "url": "/country_guessing_reactapp/static/js/runtime~main.6af80933.js"
+  },
+  {
+    "revision": "41c62db088a71dab8eba6656a59aa980",
+    "url": "/country_guessing_reactapp/static/media/world.41c62db0.jpg"
+  }
+]);
